85e6aaf94f0eebd43dd81f6df1dbfb8af3367762
Nikodem <nikodem.faber@interia.pl>
2024-08-01 13:56:17 +0200

    Changed header component

--- wypas/src/components/Header.jsx
+++ wypas/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/wypas.svg";
+import logo from "../images/logo.jpg";
 import IconFacebook from "./FacebookIcon";
 import styles from "./Header.module.scss";
 import IconInstagram from "./InstaIcon";
@@ -8,6 +8,30 @@ export default function Header() {
 	return (
 		<header className={styles.header}>
 			<div className={styles.overlay}>
+				<div className={styles.logoContainer}>
+					<img src={logo} alt="Wypas burger logo" className={styles.logoImg} />
+				</div>
+				<div className={styles.content}>
+					<h1>Wypas burger</h1>
+					<p>Najwyższej jakości burgery!</p>
+					<a href="#menu">Zobacz menu</a>
+					<div className={styles.icons}>
+						<a
+							className={styles.socialIcon}
+							href="https://www.instagram.com/wypas_burger_foodtruck/"
+							target="blank">
+							<IconInstagram />
+						</a>
+						<a
+							className={styles.socialIcon}
+							href="https://www.facebook.com/p/Wypas-Burger-100063973456060/"
+							target="blank">
+							<IconFacebook />
+						</a>
+					</div>
+				</div>
+			</div>
+			{/* <div className={styles.overlay}>
 				<a href="#"><img src={logo} alt="wypas burger logo" className={styles.logo} /></a>
 				<img src={logo} alt="wypas burger logo" className={styles.obj} />
 				<HeaderList />
@@ -31,7 +55,7 @@ export default function Header() {
 						<IconFacebook />
 					</a>
 				</div>
-			</div>
+			</div> */}
 		</header>
 	);
 }
