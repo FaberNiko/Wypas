12dd3d6ff78ece715486db5fb7b4a82510d7f8d9
Nikodem <nikodem.faber@interia.pl>
2024-08-03 11:55:38 +0200

    Updated header styles

--- wypas/src/components/Header.jsx
+++ wypas/src/components/Header.jsx
@@ -1,7 +1,6 @@
 import IconFacebook from "./FacebookIcon";
 import styles from "./Header.module.scss";
 import IconInstagram from "./InstaIcon";
-import HeaderList from "./HeaderList.jsx";
 
 export default function Header() {
 	return (
@@ -29,31 +28,6 @@ export default function Header() {
 					</div>
 				</div>
 			</div>
-			{/* <div className={styles.overlay}>
-				<a href="#"><img src={logo} alt="wypas burger logo" className={styles.logo} /></a>
-				<img src={logo} alt="wypas burger logo" className={styles.obj} />
-				<HeaderList />
-
-				<div className={styles.content}>
-					<h1>Wypas Burger</h1>
-					<p>Najwyższej jakości burgery!</p>
-					<a href="#menu">Zobacz menu</a>
-				</div>
-				<div className={styles.links}>
-					<a
-						className={styles.socialIcon}
-						href="https://www.instagram.com/wypas_burger_foodtruck/"
-						target="blank">
-						<IconInstagram />
-					</a>
-					<a
-						className={styles.socialIcon}
-						href="https://www.facebook.com/p/Wypas-Burger-100063973456060/"
-						target="blank">
-						<IconFacebook />
-					</a>
-				</div>
-			</div> */}
 		</header>
 	);
 }
