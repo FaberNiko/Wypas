c3f235038ce69f762990899172b109d526d1966f
Nikodem <nikodem.faber@interia.pl>
2024-08-03 13:48:27 +0200

    Created menu component

--- wypas/src/components/Menu.jsx
+++ wypas/src/components/Menu.jsx
@@ -7,8 +7,18 @@ export default function Menu() {
             <div className={styles.menuHero}>
                 <div className={styles.overlay}/>
                     <h2 className={styles.menuTitle}>menu</h2>
-                    <MenuList/>
-                    
+                    <MenuList  title={'klasyk'} desc={'wołowina 200g/ sałata/ pikle/ pomidor/ cebula czerwona/ prażona cebula/ sos remulada'} price={23}/>
+                    <MenuList title={'CHEDDAR'} desc={'wołowina 200g/ cheddar/ sałata/ pikle/ pomidor/ cebula czerwona/ prażona cebula/ sos remulada'} price={25}/>
+                    <MenuList title={'BEKON'} desc={'wołowina 200g/ bekon/ cheddar/ sałata/ pikle/ pomidor/ krążki cebulowe/ sos remulada/ sos BBQ'} price={28}/>
+                    <MenuList title={'MEKSYK'} desc={'wołowina 200g/ bekon/ cheddar/ nachosy/ jalapeño/ sałata/ pikle/ pomidor/ cebula czerwona/ sos remulada/ sos sambal'} price={27}/>
+                    <MenuList title={'EL DIABLO'} desc={'wołowina 200g/ bekon/ cheddar/ jalapeño/ sałata/ pikle/ pomidor/cebula czerwona/ prażona cebula/ sos sriracha/ sos habanero'} price={26}/>
+                    <MenuList title={'WYPAS'} desc={'wołowina 400g/ 2x bekon/ 2x cheddar/ sałata/ pikle/ pomidor/ cebula czerwona/ prażona cebula/ sos remulada/ sos BBQ'} price={35}/>
+                    <MenuList title={'SZTYGAR'} desc={'wołowina 200g/ sałata/ pikle/ pomidor/ cebula czerwona/ prażona cebula/ sos remulada'} price={29}/>
+                    <MenuList title={'JACK'} desc={'wołowina 200g/ 3x bekon/ cheddar/ panierowana mozarella/ sałata/pikle/ pomidor/ cebula czerwona/ sos koktajlowy z Jack&apos;iem Danielsem/ sos BBQ/ prażona cebula'} price={29}/>
+                    <MenuList title={'JUHAS'} desc={'wołowina 200g/ bekon/ oscypek/ sałata/ pikle/ pomidor/ cebula czerwona/ sos remulada/ konfitura żurawinowa'} price={29}/>
+                    <MenuList title={'HISZPAN'} desc={'wołowina 200g/ cheddar/ chorizzo/ nachosy/ sałata/ pikle/ pomidor/ cebula czerwona/ sos miodowo-musztardowy'} price={29}/>
+                    <MenuList title={'VEGE BURAK'} desc={'kotlet buraczany/ rukola pomidor/ cebula czerwona/ nasiona słonecznika/ sos żurawinowy/ sos remulada'} price={23}/>
+                    <MenuList title={'BURGER MIESIĄCA'} price={29}/>
             </div>
         </section>
     )
--- wypas/src/components/MenuList.jsx
+++ wypas/src/components/MenuList.jsx
@@ -1,101 +1,16 @@
 import styles from "./MenuList.module.scss";
 
-export default function MenuList() {
+export default function MenuList({title, desc , price}) {
 	return (
 		<ul>
 			<li className={styles.listItem}>
-				<p className={styles.title}>KLASYK</p>
+				<p className={styles.title}>{title}</p>
 				<p className={styles.description}>
-					wołowina 200g/ sałata/ pikle/ pomidor/ cebula czerwona/ prażona
-					cebula/ sos remulada
+					{desc}
 				</p>
-				<p className={styles.price}>23zł</p>
+				<p className={styles.price}>{price}zł</p>
 			</li>
-			<li className={styles.listItem}>
-				<p className={styles.title}>CHEDDAR</p>
-				<p className={styles.description}>
-					wołowina 200g/ cheddar/ sałata/ pikle/ pomidor/ cebula czerwona/
-					prażona cebula/ sos remulada
-				</p>
-				<p className={styles.price}>25zł</p>
-			</li>
-			<li className={styles.listItem}>
-				<p className={styles.title}>BEKON</p>
-				<p className={styles.description}>
-					wołowina 200g/ bekon/ cheddar/ sałata/ pikle/ pomidor/ krążki
-					cebulowe/ sos remulada/ sos BBQ
-				</p>
-				<p className={styles.price}>28zł</p>
-			</li>
-			<li className={styles.listItem}>
-				<p className={styles.title}>MEKSYK</p>
-				<p className={styles.description}>
-					wołowina 200g/ bekon/ cheddar/ nachosy/ jalapeño/ sałata/ pikle/
-					pomidor/ cebula czerwona/ sos remulada/ sos sambal
-				</p>
-				<p className={styles.price}>27zł</p>
-			</li>
-			<li className={styles.listItem}>
-				<p className={styles.title}>EL DIABLO</p>
-				<p className={styles.description}>
-					wołowina 200g/ bekon/ cheddar/ jalapeño/ sałata/ pikle/ pomidor/
-					cebula czerwona/ prażona cebula/ sos sriracha/ sos habanero
-				</p>
-				<p className={styles.price}>26zł</p>
-			</li>
-			<li className={styles.listItem}>
-				<p className={styles.title}>WYPAS</p>
-				<p className={styles.description}>
-					wołowina 400g/ 2x bekon/ 2x cheddar/ sałata/ pikle/ pomidor/ cebula
-					czerwona/ prażona cebula/ sos remulada/ sos BBQ
-				</p>
-				<p className={styles.price}>35zł</p>
-			</li>
-			<li className={styles.listItem}>
-				<p className={styles.title}>SZTYGAR</p>
-				<p className={styles.description}>
-					wołowina 200g/ bekon/ cheddar/ panierowany na czarno cheddar/ sałata/
-					pikle/ pomidor/ cebula czerwona/ sos remulada/ prażona cebula
-				</p>
-				<p className={styles.price}>29zł</p>
-			</li>
-			<li className={styles.listItem}>
-				<p className={styles.title}>JACK</p>
-				<p className={styles.description}>
-					wołowina 200g/ 3x bekon/ cheddar/ panierowana mozarella/ sałata/
-					pikle/ pomidor/ cebula czerwona/ sos koktajlowy z Jack&apos;iem
-					Danielsem/ sos BBQ/ prażona cebula
-				</p>
-				<p className={styles.price}>29zł</p>
-			</li>
-			<li className={styles.listItem}>
-				<p className={styles.title}>JUHAS</p>
-				<p className={styles.description}>
-					wołowina 200g/ bekon/ oscypek/ sałata/ pikle/ pomidor/ cebula
-					czerwona/ sos remulada/ konfitura żurawinowa
-				</p>
-				<p className={styles.price}>29zł</p>
-			</li>
-			<li className={styles.listItem}>
-				<p className={styles.title}>HISZPAN</p>
-				<p className={styles.description}>
-					wołowina 200g/ cheddar/ chorizzo/ nachosy/ sałata/ pikle/ pomidor/
-					cebula czerwona/ sos miodowo-musztardowy
-				</p>
-				<p className={styles.price}>29zł</p>
-			</li>
-			<li className={styles.listItem}>
-				<p className={styles.title}>VEGE BURAK</p>
-				<p className={styles.description}>
-					kotlet buraczany/ rukola/ pomidor/ cebula czerwona/ nasiona
-					słonecznika/ sos żurawinowy/ sos remulada
-				</p>
-				<p className={styles.price}>23zł</p>
-			</li>
-			<li className={styles.listItem}>
-				<p className={styles.title}>BURGER MIESIĄCA</p>
-				<p className={styles.price}>29zł</p>
-			</li>
+
 		</ul>
 	);
 }
