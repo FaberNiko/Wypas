177180579614b632469becebcff020bb0a6f9813
Nikodem <nikodem.faber@interia.pl>
2024-08-12 16:40:09 +0200

    Changed fonts

--- wypas/src/components/Footer.jsx
+++ wypas/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     const currentYear = new Date().getFullYear()
     return (
         <div className="footer">
-            <p class="footer__bottom-text"> Wypas Burger &copy; {currentYear} | Icons by <a href="https://icons8.com">Icons8</a></p>
+            <p className="footer__bottom-text"> Wypas Burger &copy; {currentYear} | Icons by <a href="https://icons8.com" target="blank">Icons8</a></p>
         </div>
     )
 }
