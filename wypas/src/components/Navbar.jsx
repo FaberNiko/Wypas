ef701ebe40d74ae5e73cd7ec14d7dd7e1e5f5826
Nikodem <nikodem.faber@interia.pl>
2024-08-14 16:32:15 +0200

    Changed burger button

--- wypas/src/components/Navbar.jsx
+++ wypas/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import "../App.scss";
 import logo from "../assets/logo.svg";
-import eng from '../images/eng.png'
-import polish from '../images/polish.png'
+import eng from "../images/eng.png";
+import polish from "../images/polish.png";
 import { useState } from "react";
+import { Turn as Hamburger } from "hamburger-react";
 export default function Navbar() {
 	const [isClicked, setIsClicked] = useState(false);
 	const [menuOpen, setMenuOpen] = useState(false);
@@ -14,28 +15,39 @@ export default function Navbar() {
 	return (
 		<div>
 			<nav>
-				<a href='#'className="logoContainer">
+				<a href="#" className="logoContainer">
 					<img src={logo} alt="Wypas burger logo" className="logo" />
 				</a>
-				<button className={`burgerBtn ${isClicked ?  'clicked' : ''}`} onClick={handleClick}>
-					<span></span>
-					<span></span>
-					<span></span>
-				</button>
+				<div className="burgerBtn">
+					<Hamburger
+						direction="right"
+						toggled={menuOpen}
+						toggle={setMenuOpen}
+						color="#fff"
+					/>
+				</div>
 			</nav>
 			<div className={`navList ${menuOpen ? "active" : ""}`}>
 				<ul className="nav-ul">
 					<li className="nav-ul__item">
-						<a href="#" className="nav-item" onClick={handleClick}>strona główna</a>
+						<a href="#" className="nav-item" onClick={handleClick}>
+							strona główna
+						</a>
 					</li>
 					<li className="nav-ul__item">
-						<a href="#menu" className="nav-item" onClick={handleClick}>menu</a>
+						<a href="#menu" className="nav-item" onClick={handleClick}>
+							menu
+						</a>
 					</li>
 					<li className="nav-ul__item">
-						<a href="#monthly" className="nav-item"  onClick={handleClick}>burger miesiąca</a>
+						<a href="#monthly" className="nav-item" onClick={handleClick}>
+							burger miesiąca
+						</a>
 					</li>
 					<li className="nav-ul__item">
-						<a href="#contact" className="nav-item"  onClick={handleClick}>kontakt</a>
+						<a href="#contact" className="nav-item" onClick={handleClick}>
+							kontakt
+						</a>
 					</li>
 				</ul>
 				<div className="languages">
@@ -47,8 +59,6 @@ export default function Navbar() {
 						<img src={eng} alt="english flag" />
 						<p>ENG</p>
 					</button>
-
-					
 				</div>
 			</div>
 		</div>
